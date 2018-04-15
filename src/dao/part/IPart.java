a14e06a6a204e0f6db1f5784de7257d4a67313fe
tolco97 <tolco97@abv.bg>
2018-04-15 22:03:49 +0200

    first fucking commit

---
+++ src/dao/part/IPart.java
@@ -0,0 +1,12 @@
+package dao.part;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface IPart extends Remote {
+
+	String getName() throws RemoteException;
+
+	double getWeight() throws RemoteException;
+
+}
---
+++ src/dto/part/PartDTO.java
@@ -0,0 +1,37 @@
+package dto.part;
+
+import java.io.Serializable;
+import java.rmi.RemoteException;
+
+import dao.part.IPart;
+
+public class PartDTO implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private String name;
+	private double weight;
+
+	public PartDTO(String name, double weight) {
+		this.name = name;
+		this.weight = weight;
+	}
+
+	public PartDTO(IPart part) throws RemoteException {
+		this(part.getName(), part.getWeight());
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public double getWeight() {
+		return weight;
+	}
+
+	@Override
+	public String toString() {
+		return "PartDTO [name=" + name + ", weight=" + weight + "]";
+	}
+
+}
