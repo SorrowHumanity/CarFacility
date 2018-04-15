edf23f89b0af37aa968a9ddbf71ec742659b6a7c
tolco97 <tolco97@abv.bg>
2018-04-15 22:08:39 +0200

    RemotePart implementation. I have been drinking a little bit

---
+++ src/model/part/RemotePart.java
@@ -0,0 +1,40 @@
+package model.part;
+
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+import dao.part.IPart;
+import dto.part.PartDTO;
+
+public class RemotePart extends UnicastRemoteObject implements IPart {
+
+	private static final long serialVersionUID = 1L;
+
+	private String name;
+	private double weight;
+
+	public RemotePart(String name, double weight) throws RemoteException {
+		this.name = name;
+		this.weight = weight;
+	}
+
+	public RemotePart(PartDTO partDTO) throws RemoteException {
+		this(partDTO.getName(), partDTO.getWeight());
+	}
+
+	@Override
+	public String getName() throws RemoteException {
+		return name;
+	}
+
+	@Override
+	public double getWeight() throws RemoteException {
+		return weight;
+	}
+
+	@Override
+	public String toString() {
+		return "RemotePart [name=" + name + ", weight=" + weight + "]";
+	}
+
+}
