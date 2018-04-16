301784d5addb1ef77e10d588f26df09540142669
tolco97 <tolco97@abv.bg>
2018-04-16 15:47:30 +0200

    SQL file

--- car_facility_schema.sql
+++ car_facility_schema.sql
@@ -9,5 +9,5 @@ CREATE TABLE car_facility_schema.parts (
   id                 SERIAL PRIMARY KEY UNIQUE NOT NULL,
   car_chassis_number VARCHAR(50) REFERENCES car_facility_schema.cars (chassis_number) ON DELETE SET NULL ON UPDATE CASCADE,
   name               VARCHAR(250),
-  weight_kg          NUMERIC(1)
+  weight_kg          NUMERIC(10, 3)
 );
