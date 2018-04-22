2409a7153ebc9afffe87358bde38b0ca6899faa7
tolco97 <tolco97@abv.bg>
2018-04-22 15:56:31 +0200

    Some testing done

--- src/server/main/ServerMain.java
+++ src/server/main/ServerMain.java
@@ -4,11 +4,9 @@ import java.net.MalformedURLException;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
-import java.util.Arrays;
-import java.util.List;
-
 import remote.base.dismantle_station.DismantleBaseLocator;
 import remote.base.dismantle_station.IDismantleBase;
+import remote.base.registration_station.IRegistrationBase;
 import remote.base.registration_station.RegistrationBaseLocator;
 import remote.base.shipment_station.IShipmentBase;
 import remote.base.shipment_station.ShipmentBaseLocator;
@@ -16,8 +14,6 @@ import remote.dao.car.CarDAOLocator;
 import remote.dao.pallet.PalletDAOLocator;
 import remote.dao.part.PartDAOLocator;
 import remote.dao.shipment.ShipmentDAOLocator;
-import remote.model.part.IPart;
-import util.CollectionUtils;
 
 public final class ServerMain {
 
@@ -25,10 +21,9 @@ public final class ServerMain {
 
 	public static void main(String[] args) throws RemoteException, MalformedURLException, NotBoundException {
 		bindRemoteComponents();
+		IRegistrationBase regBase = RegistrationBaseLocator.lookupBase();
 		IDismantleBase disBase = DismantleBaseLocator.lookupBase();
 		IShipmentBase shipBase = ShipmentBaseLocator.lookupBase();
-		List<IPart> parts = disBase.getAllParts();
-		shipBase.registerShipment(Arrays.asList(CollectionUtils.toDTOArray(parts)), "Anatoli", "Dvoyachki");
 	}
 
 	private static void bindRemoteComponents() throws RemoteException, MalformedURLException, NotBoundException {
